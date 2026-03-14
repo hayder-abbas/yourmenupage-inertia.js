7a9452800c377a377180538a3fcbadcd3ef632e8
hayder-abbas <haydarabbas40@proton.me>
2026-03-14 22:04:28 +0300

    fit: Update Cities grid layout

--- resources/js/Pages/Cities/Index.vue
+++ resources/js/Pages/Cities/Index.vue
@@ -10,7 +10,7 @@ defineProps<{
 
 <template>
   <div
-    class="grid grid-cols-2 md:grid-cols-3 md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto gap-3 sm:gap-4 py-8 px-4 md:px-0"
+    class="grid sm:grid-cols-2 lg:grid-cols-3 md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto gap-3 sm:gap-4 py-8 px-4 md:px-0"
   >
     <Head title="Cities" />
 
