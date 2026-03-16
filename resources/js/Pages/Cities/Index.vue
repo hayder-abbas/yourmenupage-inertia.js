dd6a8acfa23c744421d30d8cb157761170dd645c
hayder-abbas <haydarabbas40@proton.me>
2026-03-16 22:42:41 +0300

    fix: Add title to cities

--- resources/js/Pages/Cities/Index.vue
+++ resources/js/Pages/Cities/Index.vue
@@ -9,11 +9,13 @@ defineProps<{
 </script>
 
 <template>
-  <div
-    class="grid sm:grid-cols-2 lg:grid-cols-3 md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto gap-3 sm:gap-4 py-8 px-4 md:px-0"
-  >
+  <div class="md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto px-4 md:px-0">
     <Head title="Cities" />
 
-    <CardCity v-for="city in cities.data" :key="city.id" :city="city" />
+    <h1 class="py-8 font-extrabold text-4xl dark:text-white">Popular cities</h1>
+    <!-- Cities -->
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4">
+      <CardCity v-for="city in cities.data" :key="city.id" :city="city" />
+    </div>
   </div>
 </template>
