3f6ddd45b6d198e23b4872ef71d3e2b775476b2d
hayder-abbas <haydarabbas40@proton.me>
2026-03-16 23:54:11 +0300

    fit: Add 'How to works' section to home page

---
+++ resources/js/Components/Ui/Card.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  title: String,
+  desc: String,
+  img: String,
+});
+</script>
+
+<template>
+  <div class="flex items-center lg:flex-col mb-6 lg:mb-0">
+    <div>
+      <img :src="img" alt="Icon" class="w-20 object-cover" />
+    </div>
+    <div class="p-2 ml-4 lg:ml-0 lg:text-center">
+      <h3 class="font-extrabold text-lg">{{ title }}</h3>
+      <p class="font-thin text-slate-700 text-sm">
+        {{ desc }}
+      </p>
+    </div>
+  </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppHero from "@/Components/Global/AppHero.vue";
+import Card from "@/Components/Ui/Card.vue";
 import HomeLayout from "@/Layouts/HomeLayout.vue";
 import { provide } from "vue";
 
@@ -24,5 +25,33 @@ provide("canLogin", props.canLogin);
 
 <template>
   <Head title="Home" />
+  <!-- Hero -->
   <AppHero :restaurants="restaurants" :filters="filters" />
+
+  <!-- How it works -->
+  <section
+    class="md:max-w-2xl lg:max-w-4xl xl:max-w-5xl mx-auto px-4 mb-8 md:px-0 dark:text-white"
+  >
+    <h1 class="py-8 font-extrabold text-4xl dark:text-white">How it works</h1>
+
+    <div class="grid gap-4 lg:grid-cols-3">
+      <Card
+        img="/storage/kebab.png"
+        title="Discover local menus"
+        desc="See menus for popular restaurants near you."
+      />
+
+      <Card
+        img="/storage/coal.png"
+        title="Order delivery or takeout"
+        desc="Enjoy your go-to restaurant or find something new to love."
+      />
+
+      <Card
+        img="/storage/knife.png"
+        title="Easy to use anytime, anywhere"
+        desc="Menus, restaurant info, and ordering - all in one place."
+      />
+    </div>
+  </section>
 </template>
