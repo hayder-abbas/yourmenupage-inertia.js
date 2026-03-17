a95ab6a9cc409e4404eab888b9dacff3d7f7a874
hayder-abbas <haydarabbas40@proton.me>
2026-03-17 16:13:59 +0300

    fit: Add lazy loading for card-image

--- resources/js/Components/Ui/Card.vue
+++ resources/js/Components/Ui/Card.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <div class="flex items-center lg:flex-col mb-6 lg:mb-0">
     <div>
-      <img :src="img" alt="Icon" class="w-20 object-cover" />
+      <img :src="img" alt="Icon" loading="lazy" class="w-20 object-cover" />
     </div>
     <div class="p-2 ml-4 lg:ml-0 lg:text-center">
       <h3 class="font-extrabold text-lg">{{ title }}</h3>
