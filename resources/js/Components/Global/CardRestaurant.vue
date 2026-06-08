23c912ccedd61f33a35658fed40908aa221280be
hayder-abbas <haydarabbas40@proton.me>
2026-06-08 23:30:56 +0300

    fix: Not compatible with small screens

--- resources/js/Components/Global/CardRestaurant.vue
+++ resources/js/Components/Global/CardRestaurant.vue
@@ -15,42 +15,44 @@ const imgSrc = restData.restLogo
 </script>
 
 <template>
-  <Link
-    :href="route('restaurants.show', restData)"
-    class="w-full flex rounded-md border border-gray-200 dark:border-gray-600 hover:shadow-[0_0_20px_4px_#eee] dark:hover:shadow-[0_0_20px_4px_#333] dark:text-white dark:bg-gray-900 dark:hover:bg-gray-800"
-  >
-    <img
-      :src="imgSrc"
-      :alt="restData.restName"
-      class="w-[6rem] sm:w-[10rem] aspect-square object-cover h-auto rounded-s-md"
-      loading="lazy"
-    />
+  <div>
+    <Link
+      :href="route('restaurants.show', restData)"
+      class="w-full flex flex-col sm:flex-row rounded-md border border-gray-200 dark:border-gray-600 hover:shadow-[0_0_20px_4px_#eee] dark:hover:shadow-[0_0_20px_4px_#333] dark:text-white dark:bg-gray-900 dark:hover:bg-gray-800"
+    >
+      <img
+        :src="imgSrc"
+        :alt="restData.restName"
+        class="w-full sm:w-[10rem] aspect-video sm:aspect-square object-cover rounded-t-md sm:rounded-tr-none sm:rounded-s-md"
+        loading="lazy"
+      />
 
-    <div class="flex flex-col text-xl sm:grow p-5">
-      <div class="font-bold mb-2 text-amber-400">
-        {{ restData.restName }}
+      <div class="flex flex-col text-xl sm:grow p-5">
+        <div class="font-bold mb-2 text-amber-400">
+          {{ restData.restName }}
+        </div>
+        <div class="flex items-center mb-2">
+          <MapPinIcon class="w-4 h-4 mr-2" />
+          {{ restData.location }}
+        </div>
+        <div class="flex items-center mb-2">
+          <PhoneIcon class="w-4 h-4 mr-2" />
+          {{ restData.restPhone }}
+        </div>
       </div>
-      <div class="flex items-center mb-2">
-        <MapPinIcon class="w-4 h-4 mr-2" />
-        {{ restData.location }}
-      </div>
-      <div class="flex items-center mb-2">
-        <PhoneIcon class="w-4 h-4 mr-2" />
-        {{ restData.restPhone }}
-      </div>
-    </div>
 
-    <div class="w-32 p-4">
-      <div
-        class="text-center text-sm font-bold px-2 py-1 rounded-xl"
-        :class="
-          restData.isOpen
-            ? 'bg-green-100 text-green-600'
-            : 'bg-red-100 text-red-600'
-        "
-      >
-        {{ restData.isOpen ? "Open now" : "Closed" }}
+      <div class="w-32 p-4">
+        <div
+          class="text-center text-sm font-bold px-2 py-1 rounded-xl"
+          :class="
+            restData.isOpen
+              ? 'bg-green-100 text-green-600'
+              : 'bg-red-100 text-red-600'
+          "
+        >
+          {{ restData.isOpen ? "Open now" : "Closed" }}
+        </div>
       </div>
-    </div>
-  </Link>
+    </Link>
+  </div>
 </template>
