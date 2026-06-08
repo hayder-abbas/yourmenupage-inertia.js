4976120bf1696ae332fb01ea105de8865649e6b1
hayder-abbas <haydarabbas40@proton.me>
2026-06-08 22:43:38 +0300

    feat: Delete border on input focus

--- resources/js/Components/Ui/HomeSearchBar.vue
+++ resources/js/Components/Ui/HomeSearchBar.vue
@@ -33,15 +33,13 @@ watch(
   <div>
     <form class="max-w-xl mx-auto relative">
       <div class="relative">
-        <div
-          class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
-        >
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3">
           <SearchIcon class="w-4 h-4 text-gray-500 dark:text-gray-50" />
         </div>
         <input
           v-model="search"
           type="search"
-          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 dark:border-gray-500 dark:placeholder:text-gray-50 rounded-lg bg-gray-50 dark:bg-gray-400 focus:ring-blue-500 focus:border-blue-500"
+          class="block w-full p-4 ps-10 text-sm text-gray-900 dark:placeholder:text-gray-50 rounded-lg bg-gray-50 dark:bg-gray-400 focus:outline-none focus:border-transparent"
           placeholder="Search Restaurants..."
         />
       </div>
